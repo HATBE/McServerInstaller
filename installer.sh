db3ff5816c84706b4643d3895bfd8064fd140d2f
HATBE2113 <64154447+HATBE@users.noreply.github.com>
2021-08-28 22:56:16 +0200

    reworked script

--- installer.sh
+++ installer.sh
@@ -1,146 +1,200 @@
 #!/bin/bash
 
 # check out: https://github.com/Spacelord09/mcserver-deploy
-
 # Aaron Gensetter, 2021
 
-# TODO 
-#	- implement -y
-#	- Plugin selecter
+<< 'TODO'
+    L 204 -> create stop script
+    implement plugin downloader
+TODO
+
+if [[ $UID -ne 0 ]]; then
+	echo "${F_RED}You have to be root, to execute this script!${F_RESET}"
+	echo "${F_RED}try \"sudo ${0}\"${F_RESET}"
+	exit 1
+fi
+
+readonly MAX_RAM=8192
+readonly MIN_RAM=512
 
 # Text Format
-F_RED=$(tput setaf 1)
-F_GREEN=$(tput setaf 2)
-F_YELLOW=$(tput setaf 3)
-F_BLUE=$(tput setaf 4)
-F_RESET=$(tput sgr0 )
+readonly F_RED=$(tput setaf 1)
+readonly F_GREEN=$(tput setaf 2)
+readonly F_YELLOW=$(tput setaf 3)
+readonly F_BLUE=$(tput setaf 4)
+readonly F_RESET=$(tput sgr0)
 
-SRV_DIR="/opt/mc_servers"
-
-PACKAGES=(
+readonly SRV_DIR="/opt/mc_servers"
+readonly PACKAGES=(
 	jq
 	openjdk-16-jre-headless
 	screen
 	wget
 	sudo
+    curl
 )
 
-install_packages() {
-	COUNT=0
-	echo "${F_GREEN}Installing needed packages...${F_RESET}"
-	for PACKAGE in ${PACKAGES[@]}; do
-		dpkg -s $PACKAGE  &> /dev/null
-		if [[ $? -ne 0 ]]; then
-			COUNT=$(($COUNT+1))
-			echo "${F_RED}${PACKAGE} not found${F_RESET}"
-			echo "${F_GREEN}installing $PACKAGE...${F_RESET}"
-			apt install $PACKAGE -y &> /dev/null
-			if [[ $? -ne 0 ]]; then
-				echo "${F_RED}Installation failed, \"${PACKAGE}\" not found${F_RESET}"
-				echo "${F_YELLOW}try \"sudo apt update\"${F_RESET}"
-				exit 1
-			fi
-		fi
-	done
+YES=false
+SHOW_VERSIONS=false
+SILENT=false
 
-	if [[ $COUNT -eq 0 ]]; then
-		echo "${F_GREEN}Everithing up to date.${F_RESET}"
-	else 
-		echo "${F_GREEN}Installed ${F_BLUE}${COUNT}${F_GREEN} packages.${F_RESET}"
-	fi
+RAM=1024
+NAME="srv1"
+USERNAME="mc-user"
+PORT=25565
+VERSION="1.17.1"
+
+usage() {
+    echo "Usage: ${0} [-r RAM] [-n NAME] [-v VERSION] [-p PORT]"
+    echo "-----------"
+    echo "--help:           prints help"
+    echo "--show-versions:  prints all possible versions of minecraft"
+    echo "--silent;         no output to concsole"
+    echo "-----------"
+    echo "-r:               sets RAM (512 - 8192)MB (default= 1024)MB"
+    echo "-n:               sets name (default= srv1)"
+    echo "-v:               sets version (default= latest)"
+    echo "-p:               sets port (default= 25565)"
+    echo "-y:               dont ask questions"
+    exit 1
 }
 
-check_args() {
-	if [[ $# -lt 1 ]]; then
-		echo "Please use $0 --help"
-		exit 1
-	fi
+messages() {
+    if $SILENT; then
+        return
+    fi
+    local MODE=$1
+    shift
+    case "${MODE}" in
+        error) echo "${F_RED}[Error]: ${F_RESET}${@}!" >&2 ;;
+        success) echo "${F_GREEN}[Success]: ${F_RESET}${@}" ;;
+        info) echo "${F_BLUE}[Info]: ${F_RESET}${@}" ;;
+        warn) echo "${F_YELLOW}[Warn]: ${F_RESET}${@}" ;;
+        *) echo "${@}" ;;
+    esac
+}
 
-    for ARG in $@; do
-		case $ARG in
-		# HELP
-		--help|-h)
-			echo "HELP - Commands of ${0}"
-			echo "-----------"
-			echo "--help: prints Help"
-			echo "--show-versions: prints all possible versions of Minecraft"
-			echo "-----------"
-			echo "-ram: sets RAM (512 - 8192)MB (default= 1024)MB"
-			echo "-name: sets name (default= srv1)"
-			echo "-version: sets version (default= latest)"
-			echo "-port: sets port (default= 25565)"
-			echo "-y ?"
-			exit 0
-		;;
-		# SHOW VERSION
-		--show-versions|-mcv)
-			echo "Listing all PaperMC Versions."
-			echo "LATEST: $(echo $PAPERMC_VERSION_LATEST | sed -r 's/["]+//g')"
-			for VERSION in ${PAPERMC_VERSIONS[@]}; do
-				echo $(echo "-" $VERSION | sed -r 's/["]+//g')
-			done
-			exit 0
-		;;
-		-y)
-			YES=true
-		;;
-		# RAM
-		-ram=*)
-			TEMP_RAM=$(echo $ARG | sed 's/-ram=//')
+show-versions() {
+    messages 'info' 'Listing all PaperMC versions.'
+    messages 'info' "Latest version: \"$(echo $PAPERMC_VERSION_LATEST | sed -r 's/["]+//g')\""
+    for VERSION in ${PAPERMC_VERSIONS[@]}; do
+        echo "- \"$(echo $VERSION | sed -r 's/["]+//g')\""
+    done
+    exit 0
+}
 
-			if [[ $TEMP_RAM != "" ]] && [[ $TEMP_RAM -gt 511 ]] && [[ $TEMP_RAM -lt 8193 ]]; then
-				RAM=$TEMP_RAM
-				echo "${F_GREEN}- ram was set to ${F_BLUE}${RAM}MB${F_RESET}"
-			else
-				echo "${F_YELLOW}RAM not correct, set to default. ${RAM}MB${F_RESET}"
-			fi
-		;;
-		# NAME
-		-name=*)
-			TEMP_NAME=$(echo $ARG | sed 's/-name=//')
-			if [[ $TEMP_NAME != "" ]]; then
-				NAME=$TEMP_NAME
-				echo "${F_GREEN}- name was set to ${F_BLUE}${NAME}${F_RESET}"
-			else 
-				echo "${F_YELLOW}NAME not correct, set to default. ${NAME}${F_RESET}"
-			fi
-		;;
-		# VERSION
-		-version=*)
-			TEMP_VERSION=$(echo $ARG | sed 's/-version=//')
-			FOUND=0
+install_packages() {
+    local COUNT=0
+    messages 'info' "Searching needed packages"
+    for PACKAGE in ${PACKAGES[@]}; do
+        dpkg -s $PACKAGE &> /dev/null
+        if [[ $? -ne 0 ]]; then
+            COUNT=$((COUNT++))
+            messages 'warn' "\"${PACKAGE}\" not found"
+            messages 'info' "installing \"${PACKAGE}\"..."
+            apt install $PACKAGE -y &> /dev/null
+            if [[ $? -ne 0 ]]; then
+                messages 'error' "Installation of \"${PACKAGE}\" failed"
+                messages 'info' "try: sudo apt update"
+                exit 1
+            fi
+            messages 'success' "Installation of \"${PACKAGE}\" succeeded"
+        fi
+    done
 
-			for MCVERSION in ${PAPERMC_VERSIONS[@]}; do
-				if [[ ${TEMP_VERSION} == $(echo "${MCVERSION}" | sed -r 's/["]+//g') ]]; then
-					FOUND=1
-					break
-				fi
-			done
+    if [[ $COUNT -eq 0 ]]; then
+        messages 'success' "Everithing up to date"
+    else
+        messages 'success' "Installed \"${COUNT}\" packages"
+    fi
+}
 
-			if [[ $FOUND -eq 1 ]]; then
-				VERSION=$TEMP_VERSION
-				echo "${F_GREEN}version was set to ${F_BLUE}${VERSION}${F_RESET}"
-			else
-				echo "${F_YELLOW}version not found, set to default. ${VERSION}${F_RESET}"
-			fi
-		;;
-		# NAME
-		-port=*)
-			TEMP_PORT=$(echo $ARG | sed 's/-port=//')
-			if [[ $TEMP_PORT != "" ]] && [[ $TEMP_PORT -gt 1023 ]] && [[ $TEMP_PORT -lt 65536 ]]; then
-				PORT=$TEMP_PORT
-				echo "${F_GREEN}port was set to ${F_BLUE}${PORT}${F_RESET}"
-			else 
-				echo "${F_YELLOW}port not correct, set to default. ${PORT}${F_RESET}"
-			fi
-		;;
-		*);;
-		esac
+check_options() {
+    if [[ $# -lt 1 ]]; then
+        usage
+        exit 1
+    fi
+
+    while getopts r:n:v:p:-:y OPTION; do
+        case ${OPTION} in
+            -)
+                case $OPTARG in
+                    silent) SILENT=true ;;
+                    help) usage ;;
+                    show-versions) SHOW_VERSIONS=true ;;
+                esac
+            ;;
+            r) RAM=$OPTARG ;;
+            n) NAME=$OPTARG ;;
+            v) VERSION=$OPTARG ;;
+            p) PORT=$OPTARG ;;
+            y) YES=true ;;
+            *) usage ;;
+        esac
     done
 }
 
+validate() {
+    if [[ $RAM -lt $MIN_RAM || $RAM -gt $MAX_RAM ]]; then
+        messages 'error' "Please use RAM in a range of \"${MIN_RAM}MB\" and \"${MAX_RAM}MB\""
+        exit 1
+    fi
+
+    if [[ -d $SRV_DIR/mc-$NAME ]]; then
+        messages 'error' "The Server \"${NAME}\" already exists"
+        exit 1
+    fi
+
+    check_port
+    check_version
+}
+
+create_environment() {
+    if ! id "${USERNAME}" &>/dev/null; then
+		useradd -m -d $SRV_DIR $USERNAME -s /bin/bash
+		messages 'info' "User \"${USERNAME}\" was created" 
+	fi
+
+    chown $USERNAME:$USERNAME $SRV_DIR -R
+	cd $SRV_DIR
+}
+
+check_version() {
+    local FOUND=false
+
+    for MCVERSION in ${PAPERMC_VERSIONS[@]}; do
+        if [[ ${VERSION} == $(echo "${MCVERSION}" | sed -r 's/["]+//g') ]]; then
+            FOUND=true
+            break
+        fi
+    done
+
+    if ! $FOUND; then
+        messages 'warn' "version \"${VERSION}\" not found, default version: \"$PAPERMC_VERSION_LATEST\""
+        VERSION=$PAPERMC_VERSION_LATEST
+    fi
+}
+check_port() {
+	if [[ -d $SRV_DIR ]]; then
+		cd $SRV_DIR
+		local SRVS=$(ls) # -d *
+
+		for SRV in ${SRVS}; do
+			cd $SRV
+			local PORT_SELECTER=$(cat server.properties | grep server-port= | sed 's/server-port=//')
+
+			if [[ $PORT_SELECTER == $PORT  ]]; then
+				messages 'error' "Port \"${PORT}\" already in use."
+				exit 1
+			fi
+
+			cd $SRV_DIR
+		done
+	fi
+}
+
 create_service() {
-	FILE="/etc/systemd/system/mc-${NAME}.service"
+	local FILE="/etc/systemd/system/mc-${NAME}.service"
 
 	echo -e "[Unit]" >> $FILE
 	echo -e "Description=Minecraft Server: mc-$NAME" >> $FILE
@@ -157,87 +211,57 @@ create_service() {
 	echo -e "[Install]" >> $FILE
 	echo -e "WantedBy=multi-user.target" >> $FILE
 
-	echo "${F_GREEN}Service created ${F_BLUE}(mc-${NAME}.service)${F_RESET}"
+    messages 'success' "Service \"mc-${NAME}.service\" created"
 }
 
-create_enviroment() {
-	if [[ -d $SRV_DIR/mc-$NAME ]]; then
-		echo "${F_RED}This server exists! ${F_BLUE}(${NAME})${F_RESET}"
-		exit 1
-	fi
+create_server() {
+    messages 'info' "Creating server \"${NAME}\" with version \"${VERSION}\" and \"${RAM}MB\" of memory, on port \"${PORT}\""
 
-	if ! id "${USERNAME}" &>/dev/null; then
-		useradd -m -d $SRV_DIR $USERNAME -s /bin/bash
-		echo "${F_GREEN}User ${F_BLUE}\"${USERNAME}\"${F_GREEN} was created${F_RESET}"
-	fi
+    if ! $YES; then
+        read -p "Do you want to create this Server? (y/n): " READ
+        if ! [[ $READ =~ [yjYJ] ]]; then
+            messages 'error' "exiting"
+            exit 1 
+        fi
+    fi
 
-	chown $USERNAME:$USERNAME $SRV_DIR -R
-	sudo -u $USERNAME mkdir $SRV_DIR/mc-$NAME
-	echo "${F_GREEN}Created new directory for Server ${F_BLUE}mc-${NAME}${F_RESET}"
-	cd $SRV_DIR/mc-$NAME
+    sudo -u $USERNAME mkdir $SRV_DIR/mc-$NAME
+    cd $SRV_DIR/mc-$NAME
+	messages 'success' "Created directory \"${SRV_DIR}/mc-${NAME}\""
+    messages 'info' "downloading \"${VERSION}\""
+
+    wget https://papermc.io/api/v1/paper/$VERSION/latest/download -O server.jar --show-progress -q
+    echo "eula=true" > eula.txt # accept eula
+    echo "server-port=${PORT}" > server.properties # enter the user port into server config
+
+    chown $USERNAME:$USERNAME $SRV_DIR -R
+
+    create_service
+
+	systemctl enable mc-$NAME &> /dev/null
+	systemctl start mc-$NAME &> /dev/null
+    
+    if [[ $? -ne 0 ]]; then
+        messages 'success' "Service \"mc-${NAME}.service\" started"
+    fi
 }
 
-install() {
-	echo "${F_GREEN}Creating server ${F_BLUE}\"${NAME}\"${F_GREEN} with version ${F_BLUE}\"${VERSION}\"${F_GREEN} with ${F_BLUE}\"${RAM}\"MB ${F_GREEN}of memory, on port ${F_BLUE}\"${PORT}\"${F_GREEN}.${F_RESET}"
-	echo "${F_GREEN}downloading ${VERSION}...${F_RESET}"
+# SCRIPT
 
-	wget https://papermc.io/api/v1/paper/$VERSION/latest/download -O server.jar --show-progress -q
-	echo "eula=true" > eula.txt # accept eula
-	echo "server-port=${PORT}" > server.properties # set port
-
-	create_service # create a service for the minecraft server
-
-	chown $USERNAME:$USERNAME $SRV_DIR -R
-	systemctl enable mc-$NAME
-	systemctl start mc-$NAME
-}
-
-check_port() {
-	if [[ -d $SRV_DIR ]]; then
-		cd $SRV_DIR
-		SRVS=$(ls -d *)
-
-		for SRV in ${SRVS}; do
-			cd $SRV
-			PORT_SELECTER=$(cat server.properties | grep server-port= | sed 's/server-port=//')
-
-			if [[ $PORT_SELECTER == $PORT  ]]; then
-				echo "${F_RED}Port cant be set! ${F_BLUE}(${PORT})${F_RESET}"
-				exit 1
-			fi
-
-			cd $SRV_DIR
-		done
-	fi
-}
-
-##################
-# Script    	 #
-##################
-
-# check if user is root
-if [[ $UID -ne 0 ]]; then
-	echo "${F_RED}You have to be root, to execute this part!${F_RESET}"
-	echo "${F_RED}try \"sudo ${0}\"${F_RESET}"
-	exit 1
-fi
-
+check_options $@
 install_packages
 
 PAPERMC_VERSIONS=$(curl -s https://papermc.io/api/v1/paper | jq '.versions |. []')
 PAPERMC_VERSION_LATEST=$(curl -s https://papermc.io/api/v1/paper | jq '.versions |. [0]' | sed -r 's/["]+//g')
-# standard stuff
-YES=false
-RAM=1024
-NAME="srv1"
-VERSION=$PAPERMC_VERSION_LATEST
-USERNAME="mc-user"
-PORT=25565
 
-check_args $*
+# this function need to stay here (it uses packages and the papermc versions)
+if $SHOW_VERSIONS; then
+    show-versions
+    exit 0
+fi
 
-check_port # check if Port is available
-create_enviroment # create user and folder for server
-install # install the Server
+validate
+create_environment
+create_server
 
 exit 0
